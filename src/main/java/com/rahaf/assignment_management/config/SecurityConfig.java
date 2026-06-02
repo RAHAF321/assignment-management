12a7d1ee727dd7981a21c5162aa587d7ae9017c6
Rahaf Perween <rahafperween@gmail.com>
2026-06-02 23:57:17 +0530

    real authorization only GET allowed, PUT POST DELETE disabled

--- src/main/java/com/rahaf/assignment_management/config/SecurityConfig.java
+++ src/main/java/com/rahaf/assignment_management/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.rahaf.assignment_management.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,7 +16,8 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
+                        .requestMatchers(HttpMethod.GET, "/assignments/**").permitAll()
+                        .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults());
 
