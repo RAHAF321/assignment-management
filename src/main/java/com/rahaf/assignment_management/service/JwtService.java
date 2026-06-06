d96ac3f1375dfbdcc2b6bee705e3a1c86e0cc138
Rahaf Perween <rahafperween@gmail.com>
2026-06-06 23:58:43 +0530

    login page

---
+++ src/main/java/com/rahaf/assignment_management/service/JwtService.java
@@ -0,0 +1,30 @@
+package com.rahaf.assignment_management.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    private static final String SECRET =
+            "mySuperSecretKeyForJwtAuthentication123456789";
+
+    private final SecretKey key =
+            Keys.hmacShaKeyFor(SECRET.getBytes());
+
+    public String generateToken(String username) {
+
+        return Jwts.builder()
+                   .subject(username)
+                   .issuedAt(new Date())
+                   .expiration(
+                           new Date(System.currentTimeMillis() + 86400000)
+                   )
+                   .signWith(key)
+                   .compact();
+    }
+}
