80b4c1f3fd7c43f90b425096f17d8b90b83eb6c4
Rahaf Perween <rahafperween@gmail.com>
2026-06-06 23:19:37 +0530

    sample user added admin, user with unentrpted pass mech.

--- src/main/java/com/rahaf/assignment_management/config/SecurityConfig.java
+++ src/main/java/com/rahaf/assignment_management/config/SecurityConfig.java
@@ -9,6 +9,10 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 public class SecurityConfig {
@@ -16,16 +20,16 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
+        System.out.println("=== SECURITY CONFIG LOADED ===");
         http
                 .csrf(csrf -> csrf.disable())
                 .cors(cors -> {})
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/assignments/**").permitAll()
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin(login -> {})
-                .httpBasic(basic -> {});
+                .formLogin(form -> form.permitAll())
+                .logout(logout -> logout.permitAll());
 
         return http.build();
     }
@@ -55,5 +59,23 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", configuration);
 
         return source;
+    }
+
+    @Bean
+    public UserDetailsService users(){
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{noop}admin123")
+                .roles("ADMIN")
+                .build();
+        UserDetails user = User.builder()
+                .username("user")
+                .password("{noop}admin123")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(
+                admin,
+                user
+        );
     }
 }
