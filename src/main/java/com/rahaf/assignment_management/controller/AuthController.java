bb0bcebbe718aba6dbaee6b445373235a6e057ce
Rahaf Perween <rahafperween@gmail.com>
2026-06-15 00:12:49 +0530

    JWT service without validating credentials

--- src/main/java/com/rahaf/assignment_management/controller/AuthController.java
+++ src/main/java/com/rahaf/assignment_management/controller/AuthController.java
@@ -1,10 +1,24 @@
 package com.rahaf.assignment_management.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.rahaf.assignment_management.dto.LoginRequest;
+import com.rahaf.assignment_management.dto.LoginResponse;
+import com.rahaf.assignment_management.service.JwtService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
+    private final JwtService jwtService;
+
+    public AuthController(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @PostMapping("/login")
+    public LoginResponse login(@RequestBody LoginRequest request) {
+
+        String token = jwtService.generateToken(request.getUsername());
+        return new LoginResponse(token);
+    }
 }
