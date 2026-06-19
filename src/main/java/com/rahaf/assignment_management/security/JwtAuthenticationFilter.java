67f714c986e1efa6e9fbca9294cb66e4a6c1292b
Rahaf Perween <rahafperween@gmail.com>
2026-06-19 17:05:49 +0530

    Authentication filter added

--- src/main/java/com/rahaf/assignment_management/security/JwtAuthenticationFilter.java
+++ src/main/java/com/rahaf/assignment_management/security/JwtAuthenticationFilter.java
@@ -1,5 +1,27 @@
 package com.rahaf.assignment_management.security;
 
-public class JwtAuthenticationFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import lombok.RequiredArgsConstructor;
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal( HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException
+    {
+        String authHeader = request.getHeader("Authorization");
+        System.out.println("AUTH HEADER = " + authHeader);
+        filterChain.doFilter(request, response);
+    }
 }
